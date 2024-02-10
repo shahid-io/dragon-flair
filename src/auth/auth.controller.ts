2923ede66d3d86b7229dab9b0107578501d68ef9
shahid <shahid@techwarelab.com>
2024-02-10 15:40:36 +0530

    setup: docker configuration

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller("auth")
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signin')
+    login() {
+        return 'signin'
+    }
+
+    @Post('signup')
+    signup() {
+        return 'signup'
+    }
+
+}
