4280608f27d91e83340eba121e50c072ffb057e0
shahid <shahid@techwarelab.com>
2024-02-13 14:15:12 +0530

    feat: updates

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
@@ -9,23 +10,33 @@ export class AuthService {
     constructor(private prismaService: PrismaService) { }
     async signup(dto: AuthDto) {
 
-        const password = await argon.hash(dto.password);
-        console.log(password);
-        const user = await this.prismaService.user.create({
-            data: {
-                firstName: dto.firstName,
-                lastName: dto.lastName,
-                city: dto.city,
-                postalCode: dto.postalCode,
-                phone: dto.phone,
-                email: dto.email,
-                password,
-            },
-            select: {
-                id: true, firstName: true, lastName: true, email: true
+        try {
+            const password = await argon.hash(dto.password);
+            console.log(password);
+            const user = await this.prismaService.user.create({
+                data: {
+                    firstName: dto.firstName,
+                    lastName: dto.lastName,
+                    city: dto.city,
+                    postalCode: dto.postalCode,
+                    phone: dto.phone,
+                    email: dto.email,
+                    password,
+                },
+                select: {
+                    id: true, firstName: true, lastName: true, email: true
+                }
+                /**
+                 * or 
+                 * delete user.hash
+                 */
+            });
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                
             }
-        });
-        return user;
+        }
 
     }
     signin() {
