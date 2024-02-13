e7a8c64991f98e36231396dd9624d7b56f87f247
shahid <shahid@techwarelab.com>
2024-02-13 15:28:04 +0530

    feat: bug fixes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto, SignInDto } from './dto';
 import { Constants, ErrorCodes, StatusCodes } from "../utils/common"
-Controller("/auth")
+@Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) { }
 
