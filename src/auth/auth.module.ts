dc6b9c0c3ca0577146544271e7fe78193a760de7
shahid <shahid@techwarelab.com>
2024-02-15 16:36:36 +0530

    feat: added strategy and guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common"
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { JwtModule } from "@nestjs/jwt"
+import { JwtStrategy } from "./strategy"
 @Module({
     imports: [JwtModule.register({})],
-    providers: [AuthService],
     controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule { }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, } from 'passport-jwt';
+import { PrismaService } from "../../prisma/prisma.service";
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(
+        private config: ConfigService,
+        private prisma: PrismaService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: config.get('JWT_SECRET'),
+        });
+    }
+
+    async validate(payload: any) {
+        // return { userId: payload.sub, username: payload.email };
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        })
+        delete user.password;
+        return user;
+    }
+}
